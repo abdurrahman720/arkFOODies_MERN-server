2625828200b5880b8f844e4f59cabeaec1a20211
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-02 12:28:13 +0600

    mongodb intialization

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5001;
 require("dotenv").config();
 
 //middle wares
 app.use(cors());
 app.use(express.json());
+
+//mongodb database 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rtntsud.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send("ARK FOOD server running")
